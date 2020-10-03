75d0e167c07108276f73b87aa152b4f42f76ca8c
Ryan Ilham <ryan.septiyanto@smartfren.com>
2020-10-03 14:57:42 +0700

    abc

--- test/basic.js
+++ test/basic.js
@@ -1,7 +1,11 @@
 import { Selector } from 'testcafe';
 
+const slider = Selector('.ui-slider-handle.ui-corner-all.ui-state-default')
+const select = Selector('#preferred-interface')
+const option = select.find('option')
+
 fixture `My fixture`
-    .page `https://devexpress.github.io/testcafe/example/`;
+    .page (`https://devexpress.github.io/testcafe/example/`)
 
     test.skip('click populate button', async t =>{
 
@@ -30,18 +34,46 @@ fixture `My fixture`
         .click(Selector('#reusing-js-code'))
         .expect(Selector('#reusing-js-code').checked).ok()
         .click(Selector('#tried-test-cafe'))
-        .expect(Selector('#ried-test-cafe').checked).ok()
+        .expect(Selector('#tried-test-cafe').checked).ok()
 
     })
 
-    const slider = Selector('#ui-tried-test-cafe')
+  
     test.skip('geser slider ' ,async t =>{
     await t 
     .setTestSpeed(0.5)
     .click(Selector('#tried-test-cafe'))
     .drag(slider, 80, 0,{offsetX:10, offsetY:10})
-})
+    })
+  
+    test.skip('select dropdown  ' ,async t =>{
+    await t 
+    .click(select)
+    .click(option.withText('JavaScript API'))
+    .expect(select.innerText).contains('JavaScript API')
+    })
 
-
-
-    
+    test('End to End', async t => {
+        await t
+        // .setTestSpeed(0.5)
+        //click button populate
+        .setNativeDialogHandler(() => true)
+        .click(Selector('#populate'))
+        .expect(Selector('#developer-name').value).eql('Peter Parker')
+        //select radio button
+        .click(Selector('#macos'))
+        .expect(Selector('#macos').checked).ok()
+        //checkbox dan slider
+        .click(Selector('#tried-test-cafe'))
+        .drag(slider, 80, 0, {offsetX:10, offsetY:10})
+        //select dropdown
+        .click(select)
+        .click(option.withText('JavaScript API'))
+        .expect(select.innerText).contains('JavaScript API')
+        //type text di comment
+        .typeText(Selector('#comments'), 'ini di isi di comment section')
+        //click submit button
+        .click(Selector('#submit-button'))
+        //expect halaman selanjutnya
+        .expect(Selector('.result-content').innerText).contains('Peter Parker')
+    })
